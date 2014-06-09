0d9accb2a9b19899abc21f1abd657d0aeab46424
David Naylor <dnaylor@cs.cmu.edu>
2014-06-10 00:54:24 +0200

    Fixed module ordering bug

--- manager/session_manager.py
+++ manager/session_manager.py
@@ -5,11 +5,11 @@ from policy import *
 
 class DataPathState(object):
     ANY = 0
-    UNCHANGED = 0
     MUTABLE_BYTES = 1
     ADJUSTABLE_TIMING = 2
     MUTABLE_DATA = 3
     VIEWABLE_DATA = 4
+    UNCHANGED = 5
 
 class ModuleClassName(object):
     LOCAL_EXTRA = 4
@@ -29,16 +29,16 @@ MODULE_STATES = {
                                 'resulting': DataPathState.UNCHANGED,
                                 'class': ModuleClassName.LOCAL_EXTRA},
     ModuleName.TRAFFIC_SHAPING : {'required': DataPathState.ADJUSTABLE_TIMING,
-                                  'resulting': DataPathState.MUTABLE_BYTES
+                                  'resulting': DataPathState.MUTABLE_BYTES,
                                   'class': ModuleClassName.LOCAL_EXTRA},
     ModuleName.TCP             : {'required': DataPathState.ADJUSTABLE_TIMING,
-                                  'resulting': DataPathState.UNCHANGED,
+                                  'resulting': DataPathState.ADJUSTABLE_TIMING,
                                   'class': ModuleClassName.TRANSPORT},
     ModuleName.UDP             : {'required': DataPathState.ANY,
-                                  'resulting': DataPathState.UNCHANGED,
+                                  'resulting': DataPathState.ADJUSTABLE_TIMING,
                                   'class': ModuleClassName.TRANSPORT},
     ModuleName.MPTCP           : {'required': DataPathState.ADJUSTABLE_TIMING,
-                                  'resulting': DataPathState.UNCHANGED,
+                                  'resulting': DataPathState.ADJUSTABLE_TIMING,
                                   'class': ModuleClassName.TRANSPORT},
     ModuleName.IPV4            : {'required': DataPathState.MUTABLE_BYTES,
                                   'resulting': DataPathState.UNCHANGED,
@@ -61,12 +61,15 @@ class SessionManager(object):
         self.context_dict = context_dict
 
     # configure the session! returns ordered list of modules
-    def run(self, modules=None):
+    def run(self, network_modules=[], other_modules=[], nic_modules=[]):
         # TODO: get modules from module classes
+        #nic_modules = []
+        #network_modules = []
+        #other_modules = []
 
         # STEP ONE: group modules by ending state
         m_by_result = defaultdict(list)
-        for m in modules:
+        for m in other_modules:
             m_by_result[MODULE_STATES[m]['resulting']].append(m)
         print m_by_result
 
@@ -78,20 +81,19 @@ class SessionManager(object):
 
         # concatenate the lists together
         module_list = []
-        for result, modules in sorted(m_by_result.items()):
+        for result, modules in sorted(m_by_result.items(), reverse=True):
             module_list += modules
+
+        # add network and NICs
+        module_list += network_modules + nic_modules
         print module_list
 
-        # make sure the core modules are in the right order
-        # TODO
-
-        
 
 
 
 def main():
-    SessionManager().run([ModuleName.ENCRYPTION, ModuleName.COMPRESSION, ModuleName.PII_LEAK_DETECTION])
-    SessionManager().run([ModuleName.TCP, ModuleName.ENCRYPTION, ModuleName.WIFI, ModuleName.IPV4, ModuleName.COMPRESSION])
+    SessionManager().run(other_modules=[ModuleName.ENCRYPTION, ModuleName.COMPRESSION, ModuleName.PII_LEAK_DETECTION])
+    SessionManager().run(other_modules=[ModuleName.TCP, ModuleName.ENCRYPTION, ModuleName.COMPRESSION], nic_modules=[ModuleName.WIFI], network_modules=[ModuleName.IPV4])
 
 
 if __name__ == '__main__':
