b548b408e1bede844426e6ce8622f08a2c9de62b
David Naylor <dnaylor@cs.cmu.edu>
2014-06-11 23:35:45 +0200

    Added heatmap function to analyze_results

--- manager/analyze_results.py
+++ manager/analyze_results.py
@@ -1,4 +1,5 @@
 import itertools
+import matplotlib.pyplot as plt
 from collections import defaultdict
 from policy import *
 
@@ -55,6 +56,7 @@ def generate_all_possible_configurations():
         for conf in itertools.permutations(ModuleName.module_list(), i):
             yield conf
 
+
 # out of all possible data path module configurations, how many are legal
 # (as determined by test_configuration())?
 def count_legal_configurations():
@@ -67,3 +69,28 @@ def count_legal_configurations():
     print 'Num configurations (legal/total):\t%d/%d' % (legal, total)
 
 
+
+def heatmap(matrix, xlabel=None, ylabel=None, filename=None):
+    plt.pcolor(np.array(matrix), cmap='RdYlGn')
+    if xlabel: plt.xlabel(xlabel)
+    if ylabel: plt.ylabel(ylabel)
+    
+    # make sure no text is clipped along the boundaries
+    plt.tight_layout()
+
+    # FIXME hardcoded stuff that shouldn't be
+    plt.tick_params(\
+        axis='x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='off') # labels along the bottom edge are off
+    plt.tick_params(\
+        axis='y',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        left='off',
+        right='off',
+        labelleft='off')
+
+
+    plt.savefig(filename)
